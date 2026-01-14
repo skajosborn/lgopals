1acf336691ba00909e6d77e6abaf0d5f1f237c6a
skajosborn <saraosborn29@yahoo.com>
2026-01-13 20:09:55 -0500

    new manly design

--- wp-content/themes/opal-avant-garde/functions.php
+++ wp-content/themes/opal-avant-garde/functions.php
@@ -8,7 +8,7 @@ function opal_enqueue_styles() {
     // Google Fonts - Elegant pairing
     wp_enqueue_style(
         "opal-google-fonts",
-        "https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,400&family=Montserrat:wght@300;400;500;600;700;800;900&display=swap",
+        "https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Montserrat:wght@300;400;500;600;700;800;900&display=swap",
         array(),
         null
     );
